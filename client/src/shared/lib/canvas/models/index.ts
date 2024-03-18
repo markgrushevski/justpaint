979a541b3396e3fa1d282488b5714b1691351cb3
Leonid <68914627+markgrushevski@users.noreply.github.com>
2024-03-18 14:02:56 +0500

    update

--- client/src/shared/lib/canvas/models/index.ts
+++ client/src/shared/lib/canvas/models/index.ts
@@ -1,1 +1,39 @@
-export * from './CanvasEventHandler.ts'
+abstract class CanvasHandler {
+    protected constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas
+    }
+
+    protected canvas: HTMLCanvasElement
+}
+
+export abstract class CanvasEventHandlerModel extends CanvasHandler {
+    protected constructor(canvas: HTMLCanvasElement) {
+        super(canvas)
+    }
+}
+
+export abstract class CanvasToolModel extends CanvasHandler {
+    protected constructor(canvas: HTMLCanvasElement) {
+        super(canvas)
+
+        this.ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+        if (!this.ctx) throw new Error('CanvasRenderingContext2D not found')
+
+        console.log('Tool', this.ctx.lineCap)
+    }
+
+    protected ctx: CanvasRenderingContext2D
+}
+
+export abstract class CanvasWorkModel extends CanvasHandler {
+    protected constructor(canvas: HTMLCanvasElement) {
+        super(canvas)
+
+        this.ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+        if (!this.ctx) throw new Error('CanvasRenderingContext2D not found')
+
+        console.log('Work', this.ctx.lineCap)
+    }
+
+    protected ctx: CanvasRenderingContext2D
+}
